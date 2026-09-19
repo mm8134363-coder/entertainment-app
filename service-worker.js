2d90bc10d46cc245d14fd4799ab16d22fcd6e212
mm8134363-coder <mm8134363@gmail.com>
2026-09-19 11:41:49 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,143 @@
-const CACHE_NAME = "noor-al-muslim-v2";
+const CACHE_NAME = "noor-al-muslim-v3";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
   "./service-worker.js",
+
   "./icon.svg",
   "./icon-192-1.png",
   "./icon-512-1.png",
-  "./quran.json"
+
+  "./quran.json",
+
+  "./admin.html"
 ];
 
 self.addEventListener("install", function (event) {
+
   event.waitUntil(
+
     caches.open(CACHE_NAME)
+
       .then(function (cache) {
+
         return cache.addAll(FILES_TO_CACHE);
+
       })
+
       .then(function () {
+
         return self.skipWaiting();
+
       })
+
   );
+
 });
 
+
 self.addEventListener("activate", function (event) {
+
   event.waitUntil(
+
     caches.keys()
+
       .then(function (cacheNames) {
+
         return Promise.all(
+
           cacheNames.map(function (cacheName) {
+
             if (cacheName !== CACHE_NAME) {
+
               return caches.delete(cacheName);
+
             }
+
+            return null;
+
           })
+
         );
+
       })
+
+      .then(function () {
+
+        return self.clients.claim();
+
+      })
+
+  );
+
+});
+
+
+self.addEventListener("fetch", function (event) {
+
+  if (event.request.method !== "GET") {
+
+    return;
+
+  }
+
+  event.respondWith(
+
+    caches.match(event.request)
+
+      .then(function (cachedResponse) {
+
+        if (cachedResponse) {
+
+          return cachedResponse;
+
+        }
+
+        return fetch(event.request)
+
+          .then(function (networkResponse) {
+
+            if (
+              networkResponse &&
+              networkResponse.status === 200 &&
+              networkResponse.type !== "opaque"
+            ) {
+
+              const responseClone =
+                networkResponse.clone();
+
+              caches.open(CACHE_NAME)
+
+                .then(function (cache) {
+
+                  cache.put(
+                    event.request,
+                    responseClone
+                  );
+
+                });
+
+            }
+
+            return networkResponse;
+
+          })
+
+          .catch(function () {
+
+            return caches.match(
+              "./index.html"
+            );
+
+          });
+
+      })
+
+  );
+
+});      })
       .then(function () {
         return self.clients.claim();
       })
